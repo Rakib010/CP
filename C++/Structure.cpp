5ad0bde998627dbeac6afe05140ec0c4f4d40473
Rakib00 <112962930+Rakib010@users.noreply.github.com>
2023-07-25 23:37:08 +0600

    Structure.cpp

---
+++ C++/Structure.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ struct name{
+    int age;
+    char name[10];
+    float salary;
+ };
+ union dada
+ {
+    /* data */
+    int boys,girl;
+
+ };
+ 
+ int main(){
+    union dada x;
+    
+    name n;int i;
+    for(int i=0;i<5;i++){
+        cin>>n.name[i];
+    } cin>>n.age>>n.salary;  
+    cout<<n.name<<" "<<n.age<<" "<<n.salary;
+
+    cin>>x.boys>>x.girl; //union function
+    cout<<x.boys<<x. girl;
+    cout<<endl;
+   
+ }
+   
